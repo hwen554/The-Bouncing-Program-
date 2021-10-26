2971d413c9dee86b514cb0668d1ac087add39d4e
GoDamnGroove <78581470+hwen554@users.noreply.github.com>
2021-10-26 13:24:05 +1300

    Update PathType.java

--- PathType.java
+++ PathType.java
@@ -1,7 +1,7 @@
 /*
  *  ============================================================================================
  *  enum which defines the type of paths in A1
- *  YOUR UPI: hwen554 Name:Hong Weng
+ *  
  *  ============================================================================================
  */
 import java.util.*;
@@ -11,7 +11,7 @@ enum PathType { BOUNCE;
 	public static final PathType getPathType(int index) { return values()[index]; }
 	public PathType next() {
 		return values()[(ordinal() + 1) % values().length];
-		}
+	}
 	public static PathType getRandomPathType()  {
 	    return values()[rand.nextInt(SIZE)];
   }
