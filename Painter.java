e694098b60dc974c2657dba88b1998cb3f94bd45
GoDamnGroove <78581470+hwen554@users.noreply.github.com>
2021-10-26 13:23:42 +1300

    Update Painter.java

--- Painter.java
+++ Painter.java
@@ -10,9 +10,9 @@ interface Painter {
 	public void translate(int x, int y);
 	public void drawString(String text, int x, int y, int width, int height);
 	public void drawImage(Image img, int x, int y, int width, int height);
-    public void fillRect(int x, int y, int width, int height);
-    public void fillOval(int x, int y, int width, int height);
-    public void fillPolygon(Polygon p);
+    	public void fillRect(int x, int y, int width, int height);
+    	public void fillOval(int x, int y, int width, int height);
+    	public void fillPolygon(Polygon p);
 	public void setPaint(Color color);
 	public void setGraphics(Graphics g);
 	public void drawHandles(boolean isSelected, int x, int y, int width, int height);
